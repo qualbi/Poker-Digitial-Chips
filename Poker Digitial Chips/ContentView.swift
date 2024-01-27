6e0991e8618e5622a81bcef0b836dc37a376a9cb
qualbi <kevqbui6@gmail.com>
2024-01-27 05:41:12 -0500

    Added UI

--- Poker Digitial Chips/ContentView.swift
+++ Poker Digitial Chips/ContentView.swift
@@ -8,16 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var pot:Int = 0
+    var points:Int = 500
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("Poker Background")
+                .resizable()
+                .ignoresSafeArea()
+            VStack{
+                Image("Title")
+                    .resizable()
+                    .frame(width:225, height: 195)
+                
+                Spacer()
+                
+                Image("pot 4")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                
+                Text("Total Pot: $\(pot)")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                
+                Spacer()
+                
+                Text("Cash: $\(points)")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                
+                HStack{
+                    Button{
+                        
+                    }
+                label: {Image("White Chip")
+                        .resizable()
+                        .frame(width: 96, height: 96)
+                    
+                }
+                    Button{
+                        
+                    }
+                label: {Image("Red Chip")
+                        .resizable()
+                        .frame(width: 96, height: 96)
+                }
+                    Button{
+                        
+                    }
+                label: {Image("Blue Chip")
+                        .resizable()
+                        .frame(width: 96, height: 96)
+                }
+                }
+                HStack{
+                    Button{
+                        
+                    }
+                label: {Image("Green Chip")
+                        .resizable()
+                        .frame(width: 96, height: 96)
+                    
+                }
+                    Button{
+                        
+                    }
+                label: {Image("Black Chip")
+                        .resizable()
+                        .frame(width: 96, height: 96)
+                    
+                }
+                }
+            }
+                }
+
+            }
         }
-        .padding()
-    }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
