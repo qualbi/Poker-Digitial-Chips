4d011673913a6089fb42b9d5734d90ff76683589
qualbi <kevqbui6@gmail.com>
2024-01-26 23:00:48 -0500

    Initial Commit

---
+++ Poker Digitial Chips/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Poker Digitial Chips
+//
+//  Created by Kevin Bui on 1/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Poker Digitial Chips/Poker_Digitial_ChipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poker_Digitial_ChipsApp.swift
+//  Poker Digitial Chips
+//
+//  Created by Kevin Bui on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Poker_Digitial_ChipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
